11bf52258236173e7e9bfb39245ce991452de6d1
dehys <arijan.nikoci@gmail.com>
2021-03-09 20:50:50 +0100

    Region class

---
+++ src/main/java/com/dehys/regenblocks/objects/Region.java
@@ -0,0 +1,35 @@
+package com.dehys.regenblocks.objects;
+
+import java.util.List;
+
+public class Region {
+
+    private String name;
+    private Entry[] entries;
+
+    public Region(){}
+
+    public Region(String name, Entry[] entries){
+        this.name = name;
+        this.entries = entries;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Region setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Entry[] getEntries(){
+        return entries;
+    }
+
+    public Region setEntries(Entry[] entries){
+        this.entries = entries;
+        return this;
+    }
+
+}
