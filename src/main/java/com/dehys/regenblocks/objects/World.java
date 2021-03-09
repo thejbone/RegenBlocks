5e2a23d1d82d43d8580fe00d04944a8e611c8ee7
dehys <arijan.nikoci@gmail.com>
2021-03-09 20:51:01 +0100

    World class

---
+++ src/main/java/com/dehys/regenblocks/objects/World.java
@@ -0,0 +1,35 @@
+package com.dehys.regenblocks.objects;
+
+import java.util.List;
+
+public class World {
+
+    private String name;
+    private Entry[] entries;
+
+    public World(){}
+
+    public World(String name, Entry[] entries){
+        this.name = name;
+        this.entries = entries;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public World setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Entry[] getEntries(){
+        return entries;
+    }
+
+    public World setEntries(Entry[] entries){
+        this.entries = entries;
+        return this;
+    }
+
+}
