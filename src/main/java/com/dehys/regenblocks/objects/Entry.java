27a9c9ab23823c9972c141d76922f9009f3f5274
dehys <arijan.nikoci@gmail.com>
2021-03-09 20:50:36 +0100

    Entry class

---
+++ src/main/java/com/dehys/regenblocks/objects/Entry.java
@@ -0,0 +1,43 @@
+package com.dehys.regenblocks.objects;
+
+public class Entry{
+
+    private String blockName;
+    private String replacementBlock;
+    private int delay;
+
+    public Entry(){}
+
+    public Entry(String blockName, String replacementBlock, int delay){
+        this.blockName = blockName;
+        this.replacementBlock = replacementBlock;
+        this.delay = delay;
+    }
+
+    public String getBlockName() {
+        return blockName;
+    }
+
+    public Entry setBlockName(String blockName) {
+        this.blockName = blockName;
+        return this;
+    }
+
+    public String getReplacementBlock(){
+        return replacementBlock;
+    }
+
+    public Entry setReplacementBlock(String replacementBlock){
+        this.replacementBlock = replacementBlock;
+        return this;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public Entry setDelay(int delay){
+        this.delay = delay;
+        return this;
+    }
+}
