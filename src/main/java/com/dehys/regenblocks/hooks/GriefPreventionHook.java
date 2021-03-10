101856debc1efe58aedd2a1af7465309d6e559fa
dehys <arijan.nikoci@gmail.com>
2021-03-10 16:26:44 +0100

    Empty class for now

--- src/main/java/com/dehys/regenblocks/hooks/GriefPreventionHook.java
+++ src/main/java/com/dehys/regenblocks/hooks/GriefPreventionHook.java
@@ -1,4 +1,24 @@
 package com.dehys.regenblocks.hooks;
 
-public class GriefPreventionHook {
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+
+public class GriefPreventionHook implements Hook{
+
+    private GriefPrevention griefPrevention;
+    private boolean isInitialized = false;
+
+    @Override
+    public Hook Initialize() {
+        return null;
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return false;
+    }
+
+    @Override
+    public Object getInstance() {
+        return null;
+    }
 }
