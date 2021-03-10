ae3899ad644d2a6989a09ece49d62911f3830707
dehys <arijan.nikoci@gmail.com>
2021-03-10 17:17:10 +0100

    added support for worldguard and a new constructor

--- src/main/java/com/dehys/regenblocks/modules/RegenBlock.java
+++ src/main/java/com/dehys/regenblocks/modules/RegenBlock.java
@@ -1,42 +1,64 @@
-package com.dehys.regenblocks;
+package com.dehys.regenblocks.modules;
 
+import com.dehys.regenblocks.Plugin;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 
 public class RegenBlock {
 
-    private World world;
-    private Location location;
-    private Material material;
-    private long regenTime;
+    private final World world;
+    private final ProtectedRegion protectedRegion;
+    private final Location location;
+    private final Material material;
+    private final Material replacementBlock;
+    private final long regenTime;
 
-    RegenBlock(World world, Location location, Material material, long regenTime) {
+    public RegenBlock(World world, Location location, Material material, Material replacementBlock, long regenTime) {
         this.world = world;
+        this.protectedRegion = null;
         this.location = location;
         this.material = material;
+        this.replacementBlock = replacementBlock;
         this.regenTime = regenTime;
 
         Plugin.regenBlocks.add(this);
     }
 
-    void regenerate() {
+    //WorldGuard region
+    public RegenBlock(World world, ProtectedRegion region, Location location, Material material, Material replacementBlock, long regenTime) {
+        this.world = world;
+        this.protectedRegion = region;
+        this.location = location;
+        this.material = material;
+        this.replacementBlock = replacementBlock;
+        this.regenTime = regenTime;
+
+        Plugin.regenBlocks.add(this);
+    }
+
+    public void regenerate() {
         this.world.getBlockAt(this.location).setType(this.material);
     }
 
-    World getWorld() {
+    public World getWorld() {
         return this.world;
     }
 
-    Location getLocation() {
+    public Location getLocation() {
         return this.location;
     }
 
-    Material getMaterial() {
+    public Material getMaterial() {
         return this.material;
     }
 
-    long getRegenTime() {
+    public Material getReplacementBlock(){
+        return this.replacementBlock;
+    }
+
+    public long getRegenTime() {
         return this.regenTime;
     }
 }
