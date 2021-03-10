2bb7339d876e6273763fe204eb0154be1d8be0c7
dehys <arijan.nikoci@gmail.com>
2021-03-10 16:27:07 +0100

    Implemented WorldGuard hook

--- src/main/java/com/dehys/regenblocks/hooks/WorldGuardHook.java
+++ src/main/java/com/dehys/regenblocks/hooks/WorldGuardHook.java
@@ -1,4 +1,46 @@
 package com.dehys.regenblocks.hooks;
 
-public class WorldGuardHook {
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+
+public class WorldGuardHook implements Hook{
+
+    private WorldGuard worldGuard;
+    private boolean isInitialized = false;
+
+    private RegionContainer container;
+
+    @Override
+    public Hook Initialize() {
+        try{
+            worldGuard = WorldGuard.getInstance();
+            this.isInitialized = true;
+
+            container = worldGuard.getPlatform().getRegionContainer();
+
+            return this;
+        }catch (NoClassDefFoundError e){
+            this.isInitialized = false;
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    @Override
+    public Object getInstance() {
+        return this.isInitialized ? this.worldGuard : null;
+    }
+
+    public ProtectedRegion getRegion(World world, String id){
+        RegionManager regions = container.get(world);
+        return regions != null ? regions.getRegion(id) : null;
+    }
 }
