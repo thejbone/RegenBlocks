6acd860c56bda0960454fc836dea7d045c210cc9
dehys <arijan.nikoci@gmail.com>
2021-03-10 22:05:09 +0100

    added nessecary functions

--- src/main/java/com/dehys/regenblocks/modules/Entry.java
+++ src/main/java/com/dehys/regenblocks/modules/Entry.java
@@ -1,21 +1,49 @@
 package com.dehys.regenblocks.modules;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
 public class Entry{
 
+    private String id;
     private String blockName;
     private String replacementBlock;
     private int delay;
 
     public Entry(){}
 
-    public Entry(String blockName, String replacementBlock, int delay){
+    public Entry(String id, String blockName, String replacementBlock, int delay){
+        this.id = id;
         this.blockName = blockName;
         this.replacementBlock = replacementBlock;
         this.delay = delay;
     }
 
+    public String getId(){
+        return id;
+    }
+
+    public World getWorld(){
+        return Bukkit.getWorld(id.replaceAll("\\b:.+\\b", ""));
+    }
+
+    public String getRegionName(){
+        return id.replaceAll("\\b.+:\\b", "") == "" ? null : id.replaceAll("\\b.+:\\b", "");
+    }
+
+    public Entry setId(String id){
+        this.id = id;
+        return this;
+    }
+
     public String getBlockName() {
         return blockName;
+    }
+
+    public Material getBlock(){
+        return Material.getMaterial(blockName.toUpperCase());
     }
 
     public Entry setBlockName(String blockName) {
